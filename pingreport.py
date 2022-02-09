5eb9ad697ba0eb336fca0a6cce8204efc6c384ad
ftsmasaki <masaki@fts-com.jp>
2022-02-09 10:52:54 +0900

    httpレスポンス追加

--- pingreport.py
+++ pingreport.py
@@ -1,7 +1,8 @@
-### ping送信先リスト（Excelファイル）を読み込む ###
 from datetime import date
 import openpyxl
 from ping3 import ping
+import requests
+from requests import Timeout
 
 # 調査対象（Excelファイル）を指定
 wb_source=openpyxl.load_workbook('dest.xlsx')
@@ -17,26 +18,39 @@ for x in range(0, ws_source.max_row):
     for y in range(0, ws_source.max_column):
         PingList[x][y]=ws_source.cell(row=x+1, column=y+1).value
 
-### ping一斉送信 ###
+# レポート（Excelファイル）を読み込み
 wb_report=openpyxl.load_workbook(filename_report)
 ws_report=wb_report['Sheet']
 
-#ヘッダーをセルに書き込み
-ws_report.cell(row=1, column=1).value = 'IPアドレス'
-
+# 調査対象の数だけループ処理を開始
 for i in range(0, ws_source.max_row):        
-    # ping送信先IPアドレスをセルに書き込み
+    # 調査対象のIPアドレスを書き込み
     ws_report.cell(row=i+2, column=1).value=PingList[i][0]
 
-    # ping応答時間をセルに書き込み（j回送信）
+    # ping応答時間をセルに書き込み（pingをj回送信する）
     for j in range(4):
-        result=ping(PingList[i][0], unit='ms')
-        ws_report.cell(row=1, column=j+2).value = str(j+1) + '回目'
-        ws_report.cell(row=i+2, column=j+2).value=result
-        print(result)
+        #★ping送信
+        ping_result=ping(PingList[i][0], unit='ms')
+        #ping送信回数だけセルに書き込み
+        ws_report.cell(row=1, column=j+2).value=str(j+1) + '回目'
+        ws_report.cell(row=i+2, column=j+2).value=ping_result
+        #print(ping_result)
 
-### 列幅を変更 ###
+    #★HTTPリクエスト送信
+    try:
+        http_result=requests.get('http://' + PingList[i][0], timeout=3.0).status_code
+    except Timeout:
+        http_result='TimeOut'
+        pass
+    #HTTPレスポンスをセルに書き込み
+    ws_report.cell(row=i+2, column=j+3).value=http_result
+
+# ヘッダー情報をセルに書き込み
+ws_report.cell(row=1, column=1).value = 'IPアドレス'
+ws_report.cell(row=1, column=j+3).value = 'HTTPレスポンス'
+
+# 列幅を変更
 ws_report.column_dimensions['A'].width=15
 
-### レポート（Excelファイル）に保存 ###
+# レポート（Excelファイル）に保存
 wb_report.save(filename_report)
